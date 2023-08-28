074bfc1f6baf2a510d332695f39e71f954aafcf7
MiukkiK <113989655+MiukkiK@users.noreply.github.com>
2023-08-29 01:30:37 +0300

    Update HomePage.java
    Moved setVisible() calls to onConfigure overrides in respective components.
    Added final to all unchanging variables.

--- src/main/java/miukkik/freezepanes/HomePage.java
+++ src/main/java/miukkik/freezepanes/HomePage.java
@@ -24,14 +24,29 @@ public class HomePage extends FpPage {
 	private Label focusTarget;
 
 	public HomePage() {
-		Table table = getFpSession().getTable();
+		final Table table = getFpSession().getTable();
 
 		final TextField<String> editBox = new TextField<String>("editbox");
-		final Form<String> editForm = new Form<String>("editform");
+		
+		final Form<String> editForm = new Form<String>("editform") {
+
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			public void onConfigure() {
+				if (focusTarget == null) this.setVisible(false); else this.setVisible(true);
+			}	
+		};
+		
 		final AjaxLink<String> randomEdit = new AjaxLink<String>("randomedit") {
 
 			private static final long serialVersionUID = 1L;
 
+			@Override
+			public void onConfigure() {
+				if (focusTarget == null) this.setVisible(true); else this.setVisible(false);
+			}
+			
 			@Override
 			public void onClick(AjaxRequestTarget target) {
 				if (target != null) {}
@@ -47,7 +62,6 @@ public class HomePage extends FpPage {
 		
 		add(randomEdit);
 		randomEdit.setOutputMarkupPlaceholderTag(true);
-		editForm.setVisible(false);
 		add(editForm);
 		editForm.setOutputMarkupPlaceholderTag(true);
 		editForm.add(editBox);
@@ -59,11 +73,12 @@ public class HomePage extends FpPage {
 			@Override
 			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
 				if (target != null) {
-					editForm.setVisible(false);
-					randomEdit.setVisible(true);
 					target.addComponent(editForm);
 					target.addComponent(randomEdit);
 					target.addComponent(focusTarget);
+					focusTarget = null;
+					editForm.configure();
+					randomEdit.configure();
 				}
 			}
 		});
@@ -75,16 +90,17 @@ public class HomePage extends FpPage {
 			@Override
 			public void onClick(AjaxRequestTarget target) {
 				if (target != null) {
-					editForm.setVisible(false);
-					randomEdit.setVisible(true);
 					target.addComponent(editForm);
 					target.addComponent(randomEdit);
+					focusTarget = null;
+					editForm.configure();
+					randomEdit.configure();
 				}
 			}
 		});
 		
 		// Top header of the Table, height defined in FREEZE_ROWS.
-		RepeatingView headerContainer = new RepeatingView("tablehead");
+		final RepeatingView headerContainer = new RepeatingView("tablehead");
 		add(headerContainer);
 		for (int i=0; i<FREEZE_ROWS; i++) {
 			WebMarkupContainer container = new WebMarkupContainer(String.valueOf(i));
@@ -97,7 +113,7 @@ public class HomePage extends FpPage {
 
 		} 
 		// Rest of the table, first Cell of each row is part of the left header.
-		RepeatingView repeatContainer = new RepeatingView("tablebody");
+		final RepeatingView repeatContainer = new RepeatingView("tablebody");
 		add(repeatContainer);
 		for (int i=FREEZE_ROWS; i<table.getHeight(); i++) {
 			WebMarkupContainer container = new WebMarkupContainer(String.valueOf(i));
@@ -119,14 +135,13 @@ public class HomePage extends FpPage {
 					@Override
 					public void onClick(AjaxRequestTarget target) {						
 						if ((focusTarget == null)||(!label.getMarkupId().equals(focusTarget.getMarkupId()))) {
-							focusTarget = label;
-							editForm.setVisible(true);
-							randomEdit.setVisible(false);
-							editBox.setModel(model);
 							target.addComponent(randomEdit);
 							target.addComponent(editForm);
 							target.addComponent(editBox);
-							
+							focusTarget = label;
+							editBox.setModel(model);
+							editForm.configure();
+							randomEdit.configure();				
 						}
 						target.focusComponent(editBox);
 					}
